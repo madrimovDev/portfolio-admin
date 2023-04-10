3585c070973034dbb28b9695187918044e3a872c
madrimovDev <madrimov5014@gmail.com>
2023-04-10 11:19:44 +0500

    path alias

---
+++ src/app/providers.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import {RouterProvider} from 'react-router-dom'
+
+export const Providers = () => {
+  return (
+    <>
+      <RouterProvider router={[]}/>
+    </>
+  )
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "@assets/styles/index.css";
+import { Providers } from "@app";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<Providers />
+	</React.StrictMode>
+);
+
